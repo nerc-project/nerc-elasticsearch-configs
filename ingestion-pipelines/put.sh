61c32dc36e798a58374b771a1db9941ecd4659d0
Augustine Abaris <augustin@bu.edu>
2022-05-04 16:55:09 -0400

    put in a starter script to push pipeline config to elastic using api key, updated config files acordingly

---
+++ ingestion-pipelines/put.sh
@@ -0,0 +1,4 @@
+for f in nerc-osp-container-logs nerc-osp-container-logs-redis nerc-osp-container-logs-catchall
+do 
+  curl -k -K ~augustin/nerc-elk/eck-key -XPUT "https://elastic-nerc-eck.apps.nerc-shift-0.rc.fas.harvard.edu/_ingest/pipeline/$f" -H 'Content-Type: application/json' -d  @$f
+done
