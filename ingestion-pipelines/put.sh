e0fb1b1c1759c28c95df0826d3d6fa78f1c8972c
Augustine Abaris <augustin@bu.edu>
2022-05-06 15:00:39 -0400

    adding timestamp formats

--- ingestion-pipelines/put.sh
+++ ingestion-pipelines/put.sh
@@ -1,5 +1,5 @@
-#for f in nerc-osp-container-logs nerc-osp-container-logs-redis nerc-osp-container-logs-catchall
-for f in nerc-osp-container-logs
+for f in nerc-osp-container-logs nerc-osp-container-logs-redis nerc-osp-container-logs-catchall
+#for f in nerc-osp-container-logs
 do 
   curl -k -K ~augustin/nerc-elk/eck-key -XPUT "https://elastic-nerc-eck.apps.nerc-shift-0.rc.fas.harvard.edu/_ingest/pipeline/$f" -H 'Content-Type: application/json' -d  @$f
 done
