714a5c04431cfdb68de4bf2c9224f72648df90a7
Augustine Abaris <augustin@bu.edu>
2022-06-16 14:41:48 -0400

    clean up ilm and templates

--- ilm-policy/put.sh
+++
@@ -1,4 +0,0 @@
-for f in nerc-logs
-do 
-  curl -k -K ~augustin/nerc-elk/eck-key -XPUT "https://elastic-nerc-eck.apps.nerc-shift-0.rc.fas.harvard.edu/_ilm/policy/$f" -H 'Content-Type: application/json' -d  @$f
-done
